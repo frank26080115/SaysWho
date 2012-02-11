4d35c731b400bd3297aff73c38abc364408a5bba
frank26080115 <frank.zhao.main@gmail.com>
2012-02-10 19:57:49 -0500

    first commit

---
+++ pgLogin.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace SaysWho
+{
+    public partial class pgLogin : PhoneApplicationPage
+    {
+        public pgLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void brwFacebookLogin_LoadCompleted(object sender, System.Windows.Navigation.NavigationEventArgs e)
+        {
+
+        }
+
+        private void brwFacebookLogin_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
